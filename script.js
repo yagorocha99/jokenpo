87fb35799c9b64c5f40a98acf5fe87214acff079
Yago <yagovr.rocha@gmail.com>
2023-12-28 23:22:49 -0300

    Random computer choice and RoundPlay functions OK.

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-function getComputerChoice(){
-    
-}
+let getComputerChoice = () => ['Jo', 'Ken', 'Po'][Math.floor(Math.random() * 3)]
+
+let computerChoice = getComputerChoice();
+console.log("Computer's choice: " + computerChoice);
+
+let playRound = (playerSelection, computerSelection) =>
+    playerSelection = playerSelection.toLowerCase()
+    ? "It's a tie! Let's play again."
+    : (playerSelection == 'jo' && computerSelection == 'po') ||
+      (playerSelection == 'ken' && computerSelection == 'jo') ||
+      (playerSelection == 'po' && computerSelection == 'ken')
+    ? 'You Win! ${playerSelection} beats ${computerSelection}.'
+    : 'You Lose! ${computerSelection} beats ${playerSelection}.' ;
+
