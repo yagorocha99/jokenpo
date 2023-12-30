34d7af5081a36a90d0159e66ac12d3f759df839d
Yago <yagovr.rocha@gmail.com>
2023-12-29 23:44:37 -0300

    ComputerSelection wasn't returning with lowercase, now it is.

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-let getComputerChoice = () => ['Jo', 'Ken', 'Po'][Math.floor(Math.random() * 3)]
+let getComputerChoice = () => ['Jo', 'Ken', 'Po'][Math.floor(Math.random() * 3)];
 
-let computerChoice = getComputerChoice();
-console.log("Computer's choice: " + computerChoice);
+let playRound = (playerSelection, computerSelection) => {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
 
-let playRound = (playerSelection, computerSelection) =>
-    playerSelection = playerSelection.toLowerCase()
-    ? "It's a tie! Let's play again."
-    : (playerSelection == 'jo' && computerSelection == 'po') ||
-      (playerSelection == 'ken' && computerSelection == 'jo') ||
-      (playerSelection == 'po' && computerSelection == 'ken')
-    ? 'You Win! ${playerSelection} beats ${computerSelection}.'
-    : 'You Lose! ${computerSelection} beats ${playerSelection}.' ;
+    return playerSelection === computerSelection
+        ? "It's a tie! Let's play again."
+        : (playerSelection === 'jo' && computerSelection === 'po') ||
+          (playerSelection === 'ken' && computerSelection === 'jo') ||
+          (playerSelection === 'po' && computerSelection === 'ken')
+        ? `You Win! ${playerSelection} beats ${computerSelection}.`
+        : `You Lose! ${computerSelection} beats ${playerSelection}.`;
+};
 
+let playerSelection = 'po';
+let computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
