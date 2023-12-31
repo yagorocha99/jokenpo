8acae49580c3db8d8a23f61cacc1e60fd32beb43
Yago <yagovr.rocha@gmail.com>
2023-12-30 23:55:50 -0300

    Add the function Game that plays it 5 times, and a option for the user selects the option

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-let getComputerChoice = () => ['Jo', 'Ken', 'Po'][Math.floor(Math.random() * 3)];
+function game(){
+    for(let i = 1; i <= 5; i++){
+        let getComputerChoice = () => ['jo', 'ken', 'po'][Math.floor(Math.random() * 3)];
 
-let playRound = (playerSelection, computerSelection) => {
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
+        let playRound = (playerSelection, computerSelection) => {
+            playerSelection = playerSelection.toLowerCase();
+            computerSelection = computerSelection;
 
-    return playerSelection === computerSelection
-        ? "It's a tie! Let's play again."
-        : (playerSelection === 'jo' && computerSelection === 'po') ||
-          (playerSelection === 'ken' && computerSelection === 'jo') ||
-          (playerSelection === 'po' && computerSelection === 'ken')
-        ? `You Win! ${playerSelection} beats ${computerSelection}.`
-        : `You Lose! ${computerSelection} beats ${playerSelection}.`;
-};
+            return playerSelection == computerSelection
+                ? "It's a tie! Let's play again."
+                : (playerSelection == 'jo' && computerSelection == 'po') ||
+                (playerSelection == 'ken' && computerSelection == 'jo') ||
+                (playerSelection == 'po' && computerSelection == 'ken')
+                ? `You Win! ${playerSelection} beats ${computerSelection}.`
+                : `You Lose! ${computerSelection} beats ${playerSelection}.`;
+        }
 
-let playerSelection = 'po';
-let computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+        let playerSelection = prompt("Choose between Jo-Ken-Po: ");
+        let computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
+
+game();
