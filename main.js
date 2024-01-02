80756bcba44262515dfb070298cf2812039b6956
Yago <yagovr.rocha@gmail.com>
2024-01-01 23:14:55 -0300

    Defined the player and machine lives, add buttons and functions to update the html

--- main.js
+++ main.js
@@ -1,19 +1,52 @@
+let playerLives = 5;
+let machineLives = 5;
+let turnsCount = 0;
+
+document.getElementById('player').getElementsByTagName('h3')[0].textcontent = playerLives;
+document.getElementById('machine').getElementsByTagName('h3')[0].textcontent = machineLives;
+document.getElementById('turns').getElementsByTagName('h3')[0].textcontent = turnsCount;
+
+let getComputerChoice = () => ['jo', 'ken', 'po'][Math.floor(Math.random() * 3)];
+
+let updateUI = () => {
+    document.getElementById('player').getElementsByTagName('h3')[0].textcontent = playerLives;
+    document.getElementById('machine').getElementsByTagName('h3')[0].textcontent = machineLives;
+    document.getElementById('turns').getElementsByTagName('h3')[0].textcontent = turnsCount;
+
+    let resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = `<p>${result}</p>`
+}
+
+
+let playRound = (playerSelection, computerSelection) => {
+    let result;
+
+    result = playerSelection == computerSelection
+        ? "It's a tie! Let's play again."
+        : (playerSelection == 'jo' && computerSelection == 'po') ||
+        (playerSelection == 'ken' && computerSelection == 'jo') ||
+        (playerSelection == 'po' && computerSelection == 'ken')
+        ? machineLives-- && `You Win! ${playerSelection} beats ${computerSelection}.`
+        : playerLives-- && `You Lose! ${computerSelection} beats ${playerSelection}.`;
+
+    return result;
+}
+
+
+
+
+
+
+
+
+
+
+
+
 function game(){
     for(let i = 1; i <= 5; i++){
-        let getComputerChoice = () => ['jo', 'ken', 'po'][Math.floor(Math.random() * 3)];
 
-        let playRound = (playerSelection, computerSelection) => {
-            playerSelection = playerSelection.toLowerCase();
-            computerSelection = computerSelection;
-
-            return playerSelection == computerSelection
-                ? "It's a tie! Let's play again."
-                : (playerSelection == 'jo' && computerSelection == 'po') ||
-                (playerSelection == 'ken' && computerSelection == 'jo') ||
-                (playerSelection == 'po' && computerSelection == 'ken')
-                ? `You Win! ${playerSelection} beats ${computerSelection}.`
-                : `You Lose! ${computerSelection} beats ${playerSelection}.`;
-        }
+        
 
         let playerSelection = prompt("Choose between Jo-Ken-Po: ");
         let computerSelection = getComputerChoice();
